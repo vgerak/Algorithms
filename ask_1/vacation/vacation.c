dfa06b6c1850838d2fa9483050934b529c42abc2
indorilftw <vgerak@gmail.com>
2011-11-28 16:31:51 +0200

    Created directories

---
+++ ask_1/vacation/vacation.c
@@ -0,0 +1,13 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : vacation.c
+
+* Purpose : 1st exercise on Algorithms
+
+* Creation Date : 28-11-2011
+
+* Last Modified : Mon Nov 28 16:30:52 2011
+
+* Created By : Vasilis Gerakaris <vgerak@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
