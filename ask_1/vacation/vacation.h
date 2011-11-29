debb30f0587c10412440a37b0429fb818c79de8e
indorilftw <vgerak@gmail.com>
2011-11-30 01:32:32 +0200

    Added object/exec files + makefile

---
+++ ask_1/vacation/vacation.h
@@ -0,0 +1,16 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : vacation.h
+
+* Purpose :
+
+* Creation Date : 30-11-2011
+
+* Last Modified : Wed Nov 30 01:20:05 2011
+
+* Created By : Vasilis Gerakaris <vgerak@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+#include <stdio.h>
+
+unsigned int dayfinder(unsigned int pivotL, unsigned int pivotR, double avg, unsigned int days);
