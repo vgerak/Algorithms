ee7e6a1c8d4ff6208bd4dee89043ad23d8f00a28
indorilftw <vgerak@gmail.com>
2011-12-20 14:22:29 +0200

    Prepared 2nd programming exercise files and testcases

---
+++ Programming/ask2/teleport/teleport.c
@@ -0,0 +1,15 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+ * File Name : teleport.c
+
+ * Purpose : 2nd exercise in Algorithms
+
+ * Creation Date : 20-12-2011
+
+ * Last Modified : Tue Dec 20 13:04:20 2011
+
+ * Created By : Vasilis Gerakaris <vgerak@gmail.com>
+
+ _._._._._._._._._._._._._._._._._._._._._.*/
+
+
---
+++ Programming/ask2/xmasshopping/xmasshopping.c
@@ -0,0 +1,16 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+ * File Name : xmasshopping.c
+
+ * Purpose : 2nd exercise in Algorithms
+
+ * Creation Date : 20-12-2011
+
+ * Last Modified : Tue Dec 20 13:04:28 2011
+
+ * Created By : Vasilis Gerakaris <vgerak@gmail.com>
+
+ _._._._._._._._._._._._._._._._._._._._._.*/
+
+
+
