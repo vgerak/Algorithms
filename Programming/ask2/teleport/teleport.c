35882b87bf063dcfb374846f56e3a6372535fb6f
indorilftw <vgerak@gmail.com>
2012-01-07 00:49:03 +0200

    Unable to do binary search, im an idiot

--- Programming/ask2/teleport/teleport.c
+++ Programming/ask2/teleport/teleport.c
@@ -6,7 +6,7 @@
 
  * Creation Date : 20-12-2011
 
- * Last Modified : Sat Jan  7 00:20:01 2012
+ * Last Modified : Sat Jan  7 00:47:56 2012
 
  * Created By : Vasilis Gerakaris <vgerak@gmail.com>
 
@@ -25,9 +25,11 @@ typedef struct tps tlist;
 
 long int a,b;
 long int *ends, *newends;
+int *x;
+int * memstart, *temp;
 tlist *move;
 
-/* Auxiliary functions for quicksort */
+/* Auxiliary functions for quicksort & bsearch */
 int compareA(const tlist *A, const tlist *B)
 {
     if(A->start != B->start)
@@ -40,6 +42,7 @@ int compareB(const void *A, const void *B)
 {
     return (*(long int*)A - *(long int*)B);
 }
+
 /* ================================= */
 
 
@@ -51,7 +54,8 @@ int main()
     move = (tlist *) calloc(N,sizeof(tlist));
     ends = (long int*) calloc(N,sizeof(long int));
     newends = (long int*) calloc(N,sizeof(long int));
-    if (move == NULL || (ends ==NULL || (newends == NULL)))
+    x = (int*) calloc(N,sizeof(int));
+    if (move == NULL || (ends == NULL || (newends == NULL) || (x == NULL)))
     {
         printf("Out of memory\n");
         return -1;
@@ -86,6 +90,18 @@ int main()
     for(i = 0; i < ec; i++)
 	printf("Ending # %d is %ld \n", i, newends[i]);
     */
+
+    /*
+     * x[i] stores the index on newends array from the target of i
+     * newends is sorted, so binsearch for the win!
+     */
+    memstart = (int*) bsearch(&newends[0], newends, ec, sizeof(long int), compareB);
+    for (i = 0; i < N; i++)
+    {
+	temp = (int*) bsearch(&move[i].end, newends, ec, sizeof(long int), compareB);
+	x[i] = (int*) temp - (int*) memstart;
+	printf("X[%d] = %d\n", i, x[i]);
+    }
 
     return 0;
 }
