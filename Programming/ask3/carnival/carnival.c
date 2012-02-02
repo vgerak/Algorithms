9a1a95ec2eb1bfda92fa0ffb37b1dca6079b0e85
indorilftw <vgerak@gmail.com>
2012-02-02 22:15:59 +0200

    Something starts to work, needs debugging though

--- Programming/ask3/carnival/carnival.c
+++ Programming/ask3/carnival/carnival.c
@@ -6,7 +6,7 @@
 
  * Creation Date : 18-01-2012
 
- * Last Modified : Thu Feb  2 16:48:54 2012
+ * Last Modified : Thu Feb  2 22:13:21 2012
 
  * Created By : Vasilis Gerakaris <vgerak@gmail.com>
 
@@ -15,36 +15,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int V, E;  //Vertices and Edges are global
+/* Global variable Declaration */
+unsigned int V, E;
 long unsigned int res1, res2;
+unsigned int **parents;
 
-typedef struct node_t node_t;
 typedef struct edge_t edge_t;
-
 struct edge_t {
     unsigned int n1;
     unsigned int n2;
     unsigned int cost;
     unsigned short int used;
 };
-
-
-//unsigned int *nodes;
-unsigned int **parents;
 edge_t *edges;
 
-unsigned int last_i;
-
-void unify(unsigned int n1, unsigned int n2, unsigned int c)
+/* Union - Find functions */
+void unify(unsigned int n1, unsigned int n2)
 {
-    unsigned int tempn = n1;
     parents[find(n1)][0] = n2;
-    parents[n1][1] = c;
-    while (parents[tempn][1] != 0)
-    {
-        parents[n1][1] += parents[tempn][1];
-    }
-    parents[n1][1] += c;
 }
 
 int find(int n)
@@ -58,90 +46,76 @@ int find(int n)
 int findset(long unsigned int n1, long unsigned int n2)
 {
     if (find(n1) == find(n2))
-    {
-        //printf("%lu, %lu -> same set\n", n1, n2);
         return 1;
-    }
     else
-    {
-        //printf("%lu, %lu -> different set\n", n1, n2);
         return 0;
-    }
 }
 
+/* Compare function for qsort */
 int compare(const void *a, const void *b)
 {
     return ((*(edge_t *) a).cost - (*(edge_t *) b).cost);
 }
 
+/*
+ * - Implementation of Kruskal's Algorithm for MST -
+ *  We are using Union-Find to calculate it and we
+ *  are keeping the cost of the heaviest edge in
+ *  each tree, to keep data for 2nd-MST.
+ */
 long unsigned int mst()
 {
     long unsigned int cost;
     unsigned int i, num;
+
+    qsort(edges, E, sizeof(edge_t), compare);   //Sort edges by ascending weight
     cost = 0;
     num = 0;
     for (i = 0; i < E; ++i)
     {
-        //printf("i = %lu\n", i);
         if (num < V - 1)
         {
             if (findset(edges[i].n1, edges[i].n2) == 0)
             {
-                unify(edges[i].n1, edges[i].n2, edges[i].cost);
-                if (parents[edges[i].n1][0] != edges[i].n1)
-                    parents[edges[i].n1][2] = edges[i].n2;
+                unify(edges[i].n1, edges[i].n2);
+                //printf("%u now has %u as father!\n", edges[i].n1 + 1, edges[i].n2 + 1);
+                if (parents[edges[i].n1][1] < edges[i].cost)
+                    parents[edges[i].n1][1] = edges[i].cost;
                 else
-                    parents[edges[i].n2][2] = edges[i].n1;
-                printf("%u now has %u as father!\n", edges[i].n1 + 1, edges[i].n2 + 1);
+                    parents[edges[i].n2][1] = parents[edges[i].n1][1];
                 edges[i].used = 1;
-                //printf(">>> ADDED EDGE %u\n", i);
-                //printf("%u %u weight: %u\n", edges[i].n1 + 1, edges[i].n2 + 1, edges[i].cost);
                 cost += edges[i].cost;
                 num++ ;
-                last_i = i;
             }
         }
         else break;
     }
-    //printf("cost = %lu\n", cost);
     return cost;
 }
 
-unsigned int findmax(unsigned int e)
+unsigned int findmax(unsigned int a, unsigned int b)
 {
-    unsigned int i;
-    unsigned int max = 0;
-    i = edges[e].n1;
-    while (parents[i][1] != 0)
-    {
-        if (parents[i][1] > max)
-            max = parents[i][1];
-        i = parents[i][2];
-    }
-    i = edges[e].n2;
-    while (parents[i][1] != 0)
-    {
-        if (parents[i][1] > max)
-            max = parents[i][1];
-        i = parents[i][2];
-    }
+    unsigned int max = a;
+    if (max < b)
+        max = b;
     return max;
 }
 
 long unsigned int sec_mst()
 {
-    unsigned int min, test, i;
-    min = findmax(0);
-    for ( i = 0; i < E; ++i)
+    unsigned int min, temp, i;
+    min = -1;
+    for ( i = 1; i < E; ++i)
     {
+        //printf("%u\n", i);
         if (edges[i].used == 0)
         {
-            test = findmax(i);
-            if (test != 0)
-                printf("res of edge %u is %u\n", i, test);
+            //printf("min! %u\n", min);
+            temp = findmax(parents[edges[i].n1][1], parents[edges[i].n2][1]);
+            //printf("Extra w of edge %u is %u\n", i, edges[i].cost - temp);
+            if (edges[i].cost - temp < min)
+                min = edges[i].cost - temp;
         }
-        if ((test != 0) && (test < min))
-            min = test;
     }
     return (res1 + min);
 }
@@ -149,12 +123,11 @@ long unsigned int sec_mst()
 int main()
 {
     unsigned int i, j, k;
+    /* Parse input, allocate memory */
     scanf("%u %u\n", &V, &E);
     parents = (unsigned int**) calloc(V, sizeof(unsigned int*));
     for (i = 0; i <= V; ++i)
-    {
-        parents[i] = (unsigned int*) calloc(3, sizeof(unsigned int));
-    }
+        parents[i] = (unsigned int*) calloc(2, sizeof(unsigned int));
     edges = calloc(E, sizeof(edge_t));
     for (i = 0; i < E; ++i)
     {
@@ -163,18 +136,17 @@ int main()
         edges[i].n2 = k - 1;
     }
     for (i = 0; i < V; ++i)
-        parents[i][0] = parents[i][2] = i;
-
-    qsort(edges, E, sizeof(edge_t), compare);
+        parents[i][0] = i;
 
     res1 = mst();
-    printf("FOUND MST: %lu\n", res1);
-    res2 = sec_mst();
+    //printf("FOUND MST: %lu\n", res1);
     for (i = 0; i < V; ++i)
-        printf("V %u: Father %u - Weight %u\n", i + 1, parents[i][2] + 1, parents[i][1]);
+    {
+        //printf("# %u has weight %u\n", i, parents[i][1]);
+    }
+    res2 = sec_mst();
 
     printf("%lu %lu\n", res1, res2);
-
     return 0;
 }
 
